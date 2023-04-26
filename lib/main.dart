52b2628511ba0a27f5e907b6523a426b66c74209
Gabriel Hirano <69726695+gabrielhirano@users.noreply.github.com>
2023-04-26 00:35:16 -0300

    implements debug logger

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:remedio_da_hora/app/config/config_app.dart';
 import 'package:remedio_da_hora/app/dahora_app.dart';
 
 import 'src/shared/library/navigator_controller.dart';
 import 'src/shared/library/router_controller.dart';
 
 void main() {
-
+  
+  ConfigApp.setup();
   runApp(DahoraApp());
 }
