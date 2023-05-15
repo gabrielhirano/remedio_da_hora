2af9bc027249c4beccdd26bf6a10c1412a14bf19
Gabriel Hirano <69726695+gabrielhirano@users.noreply.github.com>
2023-05-15 11:05:02 -0300

    include provider

---
+++ lib/common/dahora_app.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:remedio_da_hora/src/data/data_source.dart/local_repository.dart';
+import 'package:remedio_da_hora/src/interfaces/base_repository_interface.dart';
+import 'package:remedio_da_hora/src/models/medicine_model.dart';
+import 'package:remedio_da_hora/src/screens/bottom_navigate.dart';
+import 'package:remedio_da_hora/src/screens/tratamento/tratamento_controller.dart';
+import 'package:remedio_da_hora/src/shared/library/navigator_controller.dart';
+import 'package:remedio_da_hora/src/shared/library/router_controller.dart';
+import 'package:remedio_da_hora/src/utils/global_utils.dart';
+import 'package:provider/provider.dart';
+
+class DahoraApp extends StatefulWidget {
+  @override
+  State<DahoraApp> createState() => _DahoraAppState();
+}
+
+class _DahoraAppState extends State<DahoraApp> {
+  final GlobalKey<NavigatorState> _navigatorKey = GlobalKey();
+
+  @override
+  void initState() {
+    singNavigator.setNavigatorKey(_navigatorKey);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<BaseRepository<Medicine>>(
+          create: (_) => LocalRepository<Medicine>(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) =>
+              TratamentoController(context.read<BaseRepository<Medicine>>()),
+        )
+      ],
+      child: MaterialApp(
+        locale: const Locale("pt", "BR"),
+        navigatorKey: _navigatorKey,
+        home: const BottomNavigate(),
+        // initialRoute: singRoutes.initialRoute,
+        // routes: singRoutes.routes,
+      ),
+    );
+  }
+}
