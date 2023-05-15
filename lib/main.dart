0a1a9db6117eee4e6c47d4e91c6ea111ada53c62
Gabriel Hirano <69726695+gabrielhirano@users.noreply.github.com>
2023-05-15 11:23:06 -0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:remedio_da_hora/app/config/config_app.dart';
-import 'package:remedio_da_hora/app/dahora_app.dart';
+import 'package:remedio_da_hora/common/config/config_app.dart';
+import 'package:remedio_da_hora/common/dahora_app.dart';
 
 import 'src/shared/library/navigator_controller.dart';
 import 'src/shared/library/router_controller.dart';
@@ -9,5 +9,6 @@ import 'src/shared/library/router_controller.dart';
 void main() {
   
   ConfigApp.setup();
+  
   runApp(DahoraApp());
 }
