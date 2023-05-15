c26d7a65777f224c8c4e053d9a113092d7df6dde
Gabriel Hirano <69726695+gabrielhirano@users.noreply.github.com>
2023-05-15 11:23:58 -0300

    create progresso screen into progresson module

---
+++ lib/src/screens/progresso/progresso_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ProgressoScreen extends StatefulWidget {
+  const ProgressoScreen({super.key});
+
+  @override
+  State<ProgressoScreen> createState() => _ProgressoScreenState();
+}
+
+class _ProgressoScreenState extends State<ProgressoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
