0d4b4fc074a06d948246a4842e4865b0f7913fb0
Gabriel Hirano <69726695+gabrielhirano@users.noreply.github.com>
2023-05-15 11:22:56 -0300

    create modal utils

--- lib/src/utils/global_utils.dart
+++ lib/src/utils/global_utils.dart
@@ -1,9 +1,11 @@
 import 'package:get_it/get_it.dart';
+import 'package:remedio_da_hora/src/shared/library/modal_controller.dart';
 import 'package:remedio_da_hora/src/shared/library/navigator_controller.dart';
 import 'package:remedio_da_hora/src/shared/library/router_controller.dart';
 
 final singNavigator = GetIt.I<NavigatorController>();
-final singRoutes = GetIt.I<RouterController>();
+final singRoutes = GetIt.I<RouterController>(); 
 
+final singModal = GetIt.I<ModalController>();
 
 
