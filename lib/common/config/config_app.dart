86dabc1d667909a207322a4a3e8140767605fb8c
Gabriel Hirano <69726695+gabrielhirano@users.noreply.github.com>
2023-05-15 11:04:51 -0300

    add modal singleton

--- lib/common/config/config_app.dart
+++ lib/common/config/config_app.dart
@@ -1,13 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:remedio_da_hora/src/shared/library/modal_controller.dart';
 import 'package:remedio_da_hora/src/shared/library/navigator_controller.dart';
 import 'package:remedio_da_hora/src/shared/library/router_controller.dart';
-import 'package:remedio_da_hora/src/utils/global_utils.dart';
 
 class ConfigApp {
-
-  static setup(){
+  static setup() {
     GetIt getIt = GetIt.I;
     getIt.registerSingleton(NavigatorController());
     getIt.registerSingleton(RouterController());
+    getIt.registerSingleton(ModalController());
+
   }
 }
