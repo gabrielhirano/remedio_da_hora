77626d90a492911b1e088a5d6a337585a8a60663
Gabriel Hirano <69726695+gabrielhirano@users.noreply.github.com>
2023-05-15 11:03:23 -0300

    create content type bottom sheet owner

---
+++ lib/src/widgets/bottomsheets/bottom_sheet_list_option_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:remedio_da_hora/src/utils/colors_utils.dart';
+
+class BottomSheetListOptionWidget extends StatelessWidget  with ColorsUtils{
+  const BottomSheetListOptionWidget({super.key, required this.onPressed, required this.icon, required this.text, this.textColor});
+
+  final Function() onPressed;
+  final dynamic icon;
+  final String text;
+  final Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        color: primaryBlueLight,
+        width: double.infinity,
+        height: 40,
+        child: Row(
+          children: [
+            icon,
+            const SizedBox(width: 20),
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: 14,
+                color: textColor ?? primaryBlueDark,
+                fontWeight: FontWeight.w700,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
