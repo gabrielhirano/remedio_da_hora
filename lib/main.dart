32144dc74f05535ca755b97152c070f5daa3512d
Gabriel Hirano <69726695+gabrielhirano@users.noreply.github.com>
2023-05-15 17:58:40 -0300

    adjust import main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:remedio_da_hora/common/config/config_app.dart';
+import 'package:remedio_da_hora/app/config/config_app.dart';
 import 'package:remedio_da_hora/common/dahora_app.dart';
 
 import 'src/shared/library/navigator_controller.dart';
