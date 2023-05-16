e372b66b7fe6181dbda7c6603493274c74ffc7ad
Gabriel Hirano <69726695+gabrielhirano@users.noreply.github.com>
2023-05-16 01:24:39 -0300

    register modal singleton

--- lib/app/config/config_app.dart
+++ lib/app/config/config_app.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:remedio_da_hora/src/shared/library/modal_controller.dart';
 import 'package:remedio_da_hora/src/shared/library/navigator_controller.dart';
 import 'package:remedio_da_hora/src/shared/library/router_controller.dart';
 import 'package:remedio_da_hora/src/utils/global_utils.dart';
@@ -9,5 +10,6 @@ class ConfigApp {
     GetIt getIt = GetIt.I;
     getIt.registerSingleton(NavigatorController());
     getIt.registerSingleton(RouterController());
+    getIt.registerSingleton(ModalController());
   }
 }
